d5cfe501b4bb371a9ffc464a0930adefd9329006
kubais <30256909+kubais@users.noreply.github.com>
2024-03-21 09:04:55 +0100

    Complete rework based on auliviet's creation.
    @ https://github.com/auliviet/odin-calculator/

--- script.js
+++ script.js
@@ -1,67 +1,189 @@
-let a;
-let b;
-let operator;
-let displayContent = '';
-let numberButton = document.querySelectorAll('.number');
+let firstNum = '';
+let secondNum = '';
+
+let displayValue = '';
+let currentNumber = '';
+let previousNumber = '';
+let operator = '';
+let total = '';
+
+let numbers = document.querySelectorAll('.number');
+let decimal = document.querySelector('#decimal')
+let operators = document.querySelectorAll('.operator');
 let clearButton = document.querySelector('#clear');
 let display = document.querySelector('#display');
 
-function add (a, b) {
-    return a + b;
+function add (firstNum, secondNum) {
+    return firstNum + secondNum;
 }
 
-function subtract (a, b) {
-    return a / b;
+function subtract (firstNum, secondNum) {
+    return firstNum - secondNum;
 }
 
-function multiply (a, b) {
-    return a * b;
+function multiply (firstNum, secondNum) {
+    return firstNum * secondNum;
 }
 
-function divide (a, b) {
-    return a / b;
-}
-
-
-
-
-function operate (operator, a, b) {
-    let plusButton = document.querySelector('#plus');
-    let minusButton = document.querySelector('#minus');
-    let timesButton = document.querySelector('#times');
-    let dividedButton = document.querySelector('#divided');
-
-    if (operator == 'plusButton') {
-        plusButton.addEventListener('click', add(a, b));
+function divide (firstNum, secondNum) {
+    if (secondNum != 0) {
+        return firstNum / secondNum;
     }
-    if (operator == 'minusButton') {
-        minusButton.addEventListener('click', subtract(a, b));        
-    }
-    if (operator == 'timesButton') {
-        timesButton.addEventListener('click', multiply(a,b));        
-    }
-    if (operator == 'dividedButton') {
-        dividedButton.addEventListener('click', divide(a, b));        
+    else {
+        firstNum = 'ERROR';
     }
 }
 
-operate();
 
-function populateDisplay() {
-    Array.from(numberButton).forEach(button => {
-        button.addEventListener('click', () => {
-            display.textContent += button.textContent;
-            displayContent += button.textContent;
-        })
+
+
+function operate (operator, firstNum, secondNum) {
+    
+
+    if (operator = '+') {
+        return add(firstNum, secondNum);
+    }
+    else if (operator = '-') {
+        return subtract(firstNum, secondNum);
+    }
+    else if (operator = '*') {
+        return multiply(firstNum, secondNum);
+    }
+    else if (operator = '/') {
+        return divide(firstNum, secondNum);
+    }
+    else {
+        return Error;
+    }
+}
+
+function recordNumber(input) {
+    currentNumber += input;
+}
+
+function recordOperator(input) {
+    if (previousNumber == '') {
+        previousNumber = currentNumber;
+        currentNumber = '';
+        operator = input;
+    }
+    else if (currentNumber = '') {
+        operator = input;
+    }
+    else {
+        total = operate(operator, +previousNumber, +currentNumber);
+        previousNumber = total;
+        currentNumber = '';
+        operator = input;
+    }
+}
+
+function recordFloat(input) {
+    if (currentNumber == '') {
+        currentNumber = '0.';        
+    }
+    else if (currentNumber.includes('.')) {
+
+    }
+    else {
+        currentNumber += '.';
+    }
+}
+
+function recordTotal(input) {
+    if (currentNumber != '' && previousNumber != '' && operator != '') {
+        total = operate(operator, +previousNumber, +currentNumber);
+        previousNumber = total;
+        operator = '=';
+        currentNumber = '';
+    }
+}
+
+function clearMemory() {
+    previousNumber = '';
+    currentNumber = '';
+    operator = '';
+    total = '';
+}
+
+function getInput(input) {
+    // Function to record the eventListeners and store them in a correct variable
+    
+        let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        let operations = ["+", "-", "*", "/"];
+        let equal = ["=", "Enter"];
+        let clear = ["clear"];
+        let float = ["."];
+    
+        if (numbers.includes(input)) {
+    
+            // Record all the digits entered by the user to a single number
+            recordNumber(input);
+        } 
+        else if (float.includes(input)) {
+    
+            // Add a floating point number
+    
+            recordFloat(input);
+        }
+        else if (operations.includes(input)) {
+    
+            // Record the operator entered by the user in a global variable
+            recordOperator(input);
+        }
+        else if (equal.includes(input)) {
+    
+            // Record the total of the operation
+            recordTotal(input);
+        }
+        else if (clear.includes(input)) {
+            clearMemory();
+        }
+    }
+
+
+function displayResults() {
+    let currentNumberToDisplay = '';
+
+    if (total !='' && currentNumber == '' && operator == '=') {
+        currentNumberToDisplay = total;        
+    }
+    else if (currentNumber != '') {
+        currentNumberToDisplay = currentNumber;
+    }
+    //to avoid long numbers
+    currentNumberToDisplay = currentNumberToDisplay.toString();
+
+    if (currentNumberToDisplay.length > 8) {
+        currentNumberToDisplay = currentNumberToDisplay.slice(0, 8);
+    }
+
+    //to update text content
+    document.querySelector('#current-number').textContent = currentNumberToDisplay;
+    document.querySelector('#top-row').textContent = previousNumber;
+    document.querySelector('#operator').textContent = operator;
+
+    if (currentNumberToDisplay == total && operator == '=') {
+        document.querySelector('#top-row').textContent = '';
+    }
+}
+// input for buttons
+
+buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        let input = event.target.value;
+        getInput(input);
+        displayResults();
     })
-}
+})
 
-function clearDisplay() {
-    clearButton.addEventListener('click', () => {
-        display.textContent = '';
-        displayContent = '';
-    })
-}
+// input for keyboard
 
-clearDisplay();
-populateDisplay();
+document.addEventListener('keypress', (event) => {
+    let input = event.key;
+    getInput(input);
+    displayResults();
+})
+
